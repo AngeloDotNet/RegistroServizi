383d04763ccfeb9c3b83736aab8f461b521bc339
Angelo <angelo9871@gmail.com>
2021-01-07 13:11:09 +0100

    First commit

---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RegistroServizi.Models.Options;
+using RegistroServizi.Models.Services.Infrastructure;
+
+namespace RegistroServizi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+            #if DEBUG
+            .AddRazorRuntimeCompilation()
+            #endif
+            ;
+
+            //Services
+            services.AddTransient<IApplicationPersister, ApplicationPersister>();
+
+            //Options
+            services.Configure<ApplicationOptions>(Configuration.GetSection("Applicazione"));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
+        {
+             if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+
+                lifetime.ApplicationStarted.Register(() =>
+                {
+                    string filePath = Path.Combine(env.ContentRootPath, "bin/reload.txt");
+                    File.WriteAllText(filePath, DateTime.Now.ToString());
+                });
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(routeBuilder => {
+                routeBuilder.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}

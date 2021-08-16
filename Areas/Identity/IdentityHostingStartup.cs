917d77cadf04c204e912d906fdb0a8110356d84a
Angelo Pirola <angelo@aepserver.it>
2021-08-16 21:08:42 +0200

    Eseguito refactoring del codice

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,4 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using RegistroServizi.Models.Services.Infrastructure;
 
 [assembly: HostingStartup(typeof(RegistroServizi.Areas.Identity.IdentityHostingStartup))]
 namespace RegistroServizi.Areas.Identity
--- Models/Entities/ApplicationUser.cs
+++ Models/Entities/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace RegistroServizi.Models.Entities
 {
     public class ApplicationUser : IdentityUser
     {
-    public string FullName { get; set; }
+        public string FullName { get; set; }
     }
 }

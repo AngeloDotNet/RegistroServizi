af0a1b9470e32210db77719882df84c87a349a94
Angelo Pirola <angelo@aepserver.it>
2021-08-16 20:55:11 +0200

    Aggiunta migration ApplicationUser

--- Migrations/ApplicationDb/20210816185334_IdentityApplicationUser.Designer.cs
+++ Migrations/ApplicationDb/20210816185334_IdentityApplicationUser.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroServizi.Models.Services.Infrastructure;
 
 namespace RegistroServizi.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210816185334_IdentityApplicationUser")]
+    partial class IdentityApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Services/Infrastructure/ApplicationDbContext.cs
+++ Models/Services/Infrastructure/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RegistroServizi.Models.Entities;
 
 namespace RegistroServizi.Models.Services.Infrastructure
 {
-    public partial class ApplicationDbContext : IdentityDbContext
+    public partial class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {

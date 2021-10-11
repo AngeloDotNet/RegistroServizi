4523e3d7180ffcb0bda90fb8d2f25f9c5c8606f9
Angelo Pirola <angelo9871@gmail.com>
2021-10-11 12:40:26 +0200

    Aggiunta migrations Squadra Feriale e Festivo

---
+++ Migrations/20211011103119_TabelleSquadre.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroServizi.Migrations
+{
+    public partial class TabelleSquadre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SquadraFeriale",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeSquadra = table.Column<string>(type: "TEXT", nullable: true),
+                    DescrizioneSquadra = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SquadraFeriale", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SquadraFestivo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeSquadra = table.Column<string>(type: "TEXT", nullable: true),
+                    DescrizioneSquadra = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SquadraFestivo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SquadraFeriale");
+
+            migrationBuilder.DropTable(
+                name: "SquadraFestivo");
+        }
+    }
+}

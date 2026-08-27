c3a05fa74cb108429d259d047ad61c2b2c3be9e1
Angelo Pirola <angelo9871@gmail.com>
2026-08-27 10:32:15 +0200

    Ignore local appsettings; add DI extension class
    Added appsettings.local.json to .gitignore to prevent committing local settings. Introduced DependencyInjection.cs with AddRegistroServiziApplication extension for IServiceCollection in RegistroServizi.Application. Currently, the method returns services unmodified.

---
+++ src/RegistroServizi.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RegistroServizi.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddRegistroServiziApplication(this IServiceCollection services)
+    {
+        //services.AddValidatorsFromAssemblyContaining<TValidator>();
+
+        return services;
+    }
+}

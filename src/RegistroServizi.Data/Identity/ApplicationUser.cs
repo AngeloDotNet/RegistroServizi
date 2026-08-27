6ff7ffae66f6b3728b9b0b5abaa95b8a82b5f26b
Angelo Pirola <angelo9871@gmail.com>
2026-08-27 10:32:30 +0200

    Add ApplicationUser class extending IdentityUser
    Introduced ApplicationUser class in RegistroServizi.Data.Identity to enable user profile extension. Added necessary using directives for Identity support.

---
+++ src/RegistroServizi.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace RegistroServizi.Data.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+}

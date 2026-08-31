e8d4beeca6bb50b67ff79a32b64f8bbce725e5fb
Angelo Pirola <angelo9871@gmail.com>
2026-08-31 09:34:38 +0200

    Refactor DbContext to specify user, role, and key types
    Refactored RegistroServiziDbContext to inherit from IdentityDbContext<ApplicationUser, IdentityRole, string> instead of IdentityDbContext<ApplicationUser>, enabling explicit control over user, role, and key types. Retained existing OnModelCreating logic. Previous implementation is now commented out.

--- src/RegistroServizi.Data/RegistroServiziDbContext.cs
+++ src/RegistroServizi.Data/RegistroServiziDbContext.cs
@@ -1,12 +1,22 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RegistroServizi.Data.Identity;
 
 namespace RegistroServizi.Data;
 
-public class RegistroServiziDbContext(DbContextOptions<RegistroServiziDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-{
+//public class RegistroServiziDbContext(DbContextOptions<RegistroServiziDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+//{
 
+//    protected override void OnModelCreating(ModelBuilder modelBuilder)
+//    {
+//        base.OnModelCreating(modelBuilder);
+//        modelBuilder.ApplyConfigurationsFromAssembly(typeof(RegistroServiziDbContext).Assembly);
+//    }
+//}
+
+public class RegistroServiziDbContext(DbContextOptions<RegistroServiziDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
+{
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);

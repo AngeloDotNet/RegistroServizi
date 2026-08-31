3bea76ff12c41b97817d672cb5d37371e2c3864d
Angelo Pirola <angelo9871@gmail.com>
2026-08-31 09:34:59 +0200

    Explicitly specify IdentityDbContext generic params
    Updated RegistroServiziDbContext to inherit from IdentityDbContext<ApplicationUser, IdentityRole, string> for enhanced customization of identity roles and key types. Commented out previous simpler inheritance.

--- src/RegistroServizi.Data/RegistroServiziDbContext.cs
+++ src/RegistroServizi.Data/RegistroServiziDbContext.cs
@@ -5,16 +5,6 @@ using RegistroServizi.Data.Identity;
 
 namespace RegistroServizi.Data;
 
-//public class RegistroServiziDbContext(DbContextOptions<RegistroServiziDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-//{
-
-//    protected override void OnModelCreating(ModelBuilder modelBuilder)
-//    {
-//        base.OnModelCreating(modelBuilder);
-//        modelBuilder.ApplyConfigurationsFromAssembly(typeof(RegistroServiziDbContext).Assembly);
-//    }
-//}
-
 public class RegistroServiziDbContext(DbContextOptions<RegistroServiziDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
 {
     protected override void OnModelCreating(ModelBuilder modelBuilder)

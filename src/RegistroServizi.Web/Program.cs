50b594d4aadbb11a71beb4d46f6c33e99826c276
Angelo Pirola <angelo9871@gmail.com>
2026-09-02 00:09:50 +0200

    Remove commented-out exception and HSTS config code
    Removed legacy code for exception handling and HSTS setup in non-development environments. Pipeline configuration is otherwise unchanged.

--- src/RegistroServizi.Web/Program.cs
+++ src/RegistroServizi.Web/Program.cs
@@ -80,13 +80,6 @@ public class Program
         await DatabaseInitializer.MigrateAsync(app.Services);
         app.UseForwardedHeaders();
 
-        //if (!app.Environment.IsDevelopment())
-        //{
-        //    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-        //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //    app.UseHsts();
-        //}
-
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {

0a045e3524c9e916ebe7b8f7e6050c59fff025ce
Angelo Pirola <angelo9871@gmail.com>
2026-09-06 01:35:18 +0200

    Add BaseEntity and PrezzoServizio entities
    Introduced BaseEntity abstract class with Guid Id.
    Added PrezzoServizio entity for service pricing details, inheriting from BaseEntity, with relevant properties.

---
+++ src/RegistroServizi.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace RegistroServizi.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+}
---
+++ src/RegistroServizi.Domain/Entities/PrezzoServizio.cs
@@ -0,0 +1,14 @@
+﻿using RegistroServizi.Domain.Enums;
+
+namespace RegistroServizi.Domain.Entities;
+
+public class PrezzoServizio : BaseEntity
+{
+    public TipologiaServizio TipologiaServizio { get; set; }
+    public decimal CostoFisso { get; set; }
+    public decimal CostoKm { get; set; }
+    public decimal SecondoTrasportato { get; set; }
+    public decimal FermoMacchina { get; set; }
+    public decimal? Accompagnatore { get; set; }
+    public int? ScontoSocio { get; set; }
+}

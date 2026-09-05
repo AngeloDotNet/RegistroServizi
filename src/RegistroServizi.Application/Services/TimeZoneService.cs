0f92939b0a6a29bd9c2639f0412468bfab494d9c
Angelo Pirola <angelo9871@gmail.com>
2026-09-05 09:53:16 +0200

    Add TimeZoneService to resolve user's time zone from header
    Implemented TimeZoneService using IHttpContextAccessor to read the "x-time-zone" header and resolve it to a TimeZoneInfo object. Returns null if the header is missing or invalid.

---
+++ src/RegistroServizi.Application/Services/TimeZoneService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using RegistroServizi.Application.Interfaces;
+
+namespace RegistroServizi.Application.Services;
+
+public class TimeZoneService(IHttpContextAccessor httpContextAccessor) : ITimeZoneService
+{
+    public static readonly string HeaderKey = "x-time-zone";
+
+    public TimeZoneInfo? GetTimeZone()
+    {
+        var timeZoneId = GetTimeZoneHeaderValue();
+
+        if (timeZoneId is null || !TimeZoneInfo.TryFindSystemTimeZoneById(timeZoneId, out var timeZoneInfo))
+        {
+            return null;
+        }
+
+        return timeZoneInfo;
+    }
+
+    public string? GetTimeZoneHeaderValue()
+    {
+        if (httpContextAccessor.HttpContext?.Request?.Headers?.TryGetValue(HeaderKey, out var timeZone) == true)
+        {
+            return timeZone.ToString();
+        }
+
+        return null;
+    }
+}

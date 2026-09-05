3eea95897f2572ad2b3cc57436288c8439c3d408
Angelo Pirola <angelo9871@gmail.com>
2026-09-05 09:53:09 +0200

    Add ClientTimeProvider with DI-based time zone support
    Introduced ClientTimeProvider in RegistroServizi.Application.Services, inheriting from TimeProvider. Uses ITimeZoneService via dependency injection to determine LocalTimeZone, defaulting to UTC if not provided.

---
+++ src/RegistroServizi.Application/Services/ClientTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using RegistroServizi.Application.Interfaces;
+
+namespace RegistroServizi.Application.Services;
+
+public class ClientTimeProvider(ITimeZoneService timeZoneService) : TimeProvider
+{
+    public override TimeZoneInfo LocalTimeZone => timeZoneService.GetTimeZone() ?? TimeZoneInfo.Utc;
+}

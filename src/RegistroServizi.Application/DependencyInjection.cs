e13e1d446f104ff9ebd6aeabc4f33c3fb5a08b55
Angelo Pirola <angelo9871@gmail.com>
2026-09-05 09:53:29 +0200

    Register time-related services for DI
    Added TimeProvider.System, ClientTimeProvider, and ITimeZoneService (with TimeZoneService implementation) as singletons to the DI container to support time-based dependency injection.

--- src/RegistroServizi.Application/DependencyInjection.cs
+++ src/RegistroServizi.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using RegistroServizi.Application.Interfaces;
+using RegistroServizi.Application.Services;
 
 namespace RegistroServizi.Application;
 
@@ -6,6 +8,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddRegistroServiziApplication(this IServiceCollection services)
     {
+        services.AddSingleton(TimeProvider.System);
+        services.AddSingleton<ClientTimeProvider>();
+        services.AddSingleton<ITimeZoneService, TimeZoneService>();
+
         //services.AddValidatorsFromAssemblyContaining<TValidator>();
 
         return services;

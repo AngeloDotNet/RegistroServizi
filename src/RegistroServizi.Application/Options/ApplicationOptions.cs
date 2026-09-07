404031ce30fc17b35f340e8e8b1b0bfb1e9d418c
Angelo Pirola <angelo9871@gmail.com>
2026-09-07 10:01:14 +0200

    Remove obsolete ApplicationOptions.cs file
    The ApplicationOptions.cs file, which contained only a commented-out class definition, has been deleted as it was no longer needed.

--- src/RegistroServizi.Application/Options/ApplicationOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RegistroServizi.Application.Options;
-
-//public class ApplicationOptions
-//{
-//    public string Name { get; set; } = string.Empty;
-//    public string Version { get; set; } = string.Empty;
-//}

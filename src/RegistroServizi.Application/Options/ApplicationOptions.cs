dc5dcae0cc50254f3e7e9278286d8d4f0807ff58
Angelo Pirola <angelo9871@gmail.com>
2026-09-07 10:00:55 +0200

    Add namespace; comment out ApplicationOptions class
    Added RegistroServizi.Application.Options namespace. Commented out the ApplicationOptions class and its properties (Name, Version) to disable the class without removing its code.

---
+++ src/RegistroServizi.Application/Options/ApplicationOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace RegistroServizi.Application.Options;
+
+//public class ApplicationOptions
+//{
+//    public string Name { get; set; } = string.Empty;
+//    public string Version { get; set; } = string.Empty;
+//}

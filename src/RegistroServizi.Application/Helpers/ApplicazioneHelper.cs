13d7545694ae409d64814d78232988b4bb869fb1
Angelo Pirola <angelo9871@gmail.com>
2026-09-08 09:49:09 +0200

    Add helper classes for entity-to-DTO mapping
    Added ApplicazioneHelper and PrezzoServizioHelper in RegistroServizi.Application.Helpers to map Applicazione and PrezzoServizio entities to their respective DTOs. Included XML documentation for methods and left commented code for potential detail DTO mappings.

---
+++ src/RegistroServizi.Application/Helpers/ApplicazioneHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace RegistroServizi.Application.Helpers;
+
+/// <summary>
+/// Helper class for mapping Applicazione entities to their corresponding DTOs.
+/// </summary>
+public static class ApplicazioneHelper
+{
+    /// <summary>
+    /// Maps an Applicazione entity to an ApplicazioneDto.
+    /// </summary>
+    /// <param name="applicazione"></param>
+    /// <returns></returns>
+    public static ApplicazioneDto MapApplicazioneToDto(Applicazione applicazione) => new ApplicazioneDto(applicazione.Id, applicazione.NomeApplicazione, applicazione.Versione);
+
+    /// <summary>
+    /// Maps an Applicazione entity to a DetailApplicazioneDto.
+    /// </summary>
+    /// <param name="applicazione"></param>
+    /// <returns></returns>
+    //public static DetailApplicazioneDto MapApplicazioneToDetailDto(Applicazione applicazione) => new DetailApplicazioneDto(applicazione.Id, applicazione.NomeApplicazione, applicazione.Versione);
+}
---
+++ src/RegistroServizi.Application/Helpers/PrezzoServizioHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace RegistroServizi.Application.Helpers;
+
+/// <summary>
+/// Helper class for mapping PrezzoServizio entities to DTOs.
+/// </summary>
+public static class PrezzoServizioHelper
+{
+    /// <summary>
+    /// Maps a PrezzoServizio entity to a PrezzoServizioDto.
+    /// </summary>
+    /// <param name="prezzoServizio"></param>
+    /// <returns></returns>
+    public static PrezzoServizioDto MapPrezzoServizioToDto(PrezzoServizio prezzoServizio) => new PrezzoServizioDto
+    {
+        Id = prezzoServizio.Id,
+        TipologiaServizio = prezzoServizio.TipologiaServizio,
+        CostoFisso = prezzoServizio.CostoFisso,
+        CostoKm = prezzoServizio.CostoKm,
+        SecondoTrasportato = prezzoServizio.SecondoTrasportato,
+        FermoMacchina = prezzoServizio.FermoMacchina,
+        Accompagnatore = prezzoServizio.Accompagnatore,
+        ScontoSocio = prezzoServizio.ScontoSocio
+    };
+
+    /// <summary>
+    /// Maps a PrezzoServizio entity to a PrezzoServizioDetailDto.
+    /// </summary>
+    /// <param name="prezzoServizio"></param>
+    /// <returns></returns>
+    //public static DetailPrezzoServizioDto MapPrezzoServizioToDetailDto(PrezzoServizio prezzoServizio) => new DetailPrezzoServizioDto(prezzoServizio.Id,
+    //    prezzoServizio.TipologiaServizio, prezzoServizio.CostoFisso, prezzoServizio.CostoKm, prezzoServizio.SecondoTrasportato,
+    //    prezzoServizio.FermoMacchina, prezzoServizio.Accompagnatore, prezzoServizio.ScontoSocio);
+}

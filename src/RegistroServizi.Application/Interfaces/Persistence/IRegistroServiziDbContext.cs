ce518e3d525a5fbe6c4e760615178077b1010f97
Angelo Pirola <angelo9871@gmail.com>
2026-09-08 09:49:50 +0200

    Add IRegistroServiziDbContext interface with DbSets
    Introduced IRegistroServiziDbContext in RegistroServizi.Application.Interfaces.Persistence. Defines DbSet properties for PrezziServizi, Applicazioni, and Personalizzazioni, plus async SaveChangesAsync method.

---
+++ src/RegistroServizi.Application/Interfaces/Persistence/IRegistroServiziDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistroServizi.Application.Interfaces.Persistence;
+
+public interface IRegistroServiziDbContext
+{
+    DbSet<PrezzoServizio> PrezziServizi { get; }
+    DbSet<Applicazione> Applicazioni { get; }
+    DbSet<Personalizzazione> Personalizzazioni { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}

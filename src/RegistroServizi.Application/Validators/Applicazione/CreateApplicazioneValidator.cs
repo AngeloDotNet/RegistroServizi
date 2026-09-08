3cb92d14ce759a6e9a2d868c8cbcdd976a208150
Angelo Pirola <angelo9871@gmail.com>
2026-09-08 09:53:04 +0200

    Add commented validator code for Applicazione DTOs
    Added full, commented-out implementations of CreateApplicazioneValidator and UpdateApplicazioneValidator using FluentValidation. The rules validate NomeApplicazione and Versione properties for non-empty values, max lengths, and version format. No active code changes were made.

---
+++ src/RegistroServizi.Application/Validators/Applicazione/CreateApplicazioneValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace RegistroServizi.Application.Validators.Applicazione;
+
+//public class CreateApplicazioneValidator : AbstractValidator<CreateApplicazioneDto>
+//{
+//    //public CreateApplicazioneValidator()
+//    //{
+//    //    RuleFor(x => x.NomeApplicazione)
+//    //        .NotEmpty().WithMessage("NomeApplicazione non può essere vuoto.")
+//    //        .MaximumLength(200).WithMessage("NomeApplicazione non può superare i 200 caratteri.");
+
+//    //    RuleFor(x => x.Versione)
+//    //        .NotEmpty().WithMessage("Versione non può essere vuoto.")
+//    //        .MaximumLength(15).WithMessage("Versione non può superare i 15 caratteri.");
+//    //}
+
+//    private static readonly Regex versioneRegex = new(@"^\d+\.\d+\.\d+$", RegexOptions.Compiled);
+
+//    public CreateApplicazioneValidator()
+//    {
+//        RuleFor(x => x.NomeApplicazione)
+//            .NotEmpty().WithMessage("NomeApplicazione non può essere vuoto.")
+//            .MaximumLength(200).WithMessage("NomeApplicazione non può superare i 200 caratteri.");
+
+//        RuleFor(x => x.Versione)
+//            .NotEmpty().WithMessage("Versione non può essere vuoto.")
+//            .MaximumLength(15).WithMessage("Versione non può superare i 15 caratteri.")
+//            .Must(v => versioneRegex.IsMatch(v))
+//            .WithMessage("Versione deve avere il formato X.Y.Z, ad esempio 1.0.0.");
+//    }
+//}

1f9bce35c4939719ff636d678221d99850045d2c
Angelo Pirola <angelo9871@gmail.com>
2026-09-08 09:50:33 +0200

    Remove commented-out method signatures from interfaces
    The commented-out GetApplicazioneDetailAsync and GetPrezzoServizioDetailAsync method signatures were removed from IApplicazioneService and IPrezzoServizioService interfaces. No other changes were made.

--- src/RegistroServizi.Application/Interfaces/Services/IPrezzoServizioService.cs
+++ src/RegistroServizi.Application/Interfaces/Services/IPrezzoServizioService.cs
@@ -4,7 +4,6 @@ public interface IPrezzoServizioService
 {
     Task<IReadOnlyList<PrezzoServizioDto>> GetAllPrezziServiziAsync(CancellationToken cancellationToken = default);
     Task<PrezzoServizioDto> GetByIdPrezzoServizioAsync(Guid id, CancellationToken cancellationToken = default);
-    //Task<DetailPrezzoServizioDto> GetPrezzoServizioDetailAsync(Guid id, CancellationToken cancellationToken = default);
     Task<PrezzoServizioDto> CreatePrezzoServizioAsync(CreatePrezzoServizioDto createDto, CancellationToken cancellationToken = default);
     Task<PrezzoServizioDto> UpdatePrezzoServizioAsync(UpdatePrezzoServizioDto updateDto, CancellationToken cancellationToken = default);
 }

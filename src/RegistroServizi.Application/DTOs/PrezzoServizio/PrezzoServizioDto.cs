900104b69023016dbeb590450d63499801d29bba
Angelo Pirola <angelo9871@gmail.com>
2026-09-08 09:48:53 +0200

    Refactor PrezzoServizioDto to standard class, update docs
    Updated the summary comment to a concise Italian description. Removed XML parameter documentation. Changed PrezzoServizioDto from a record class to a standard class with properties and public setters. Removed the commented-out record class definition.

--- src/RegistroServizi.Application/DTOs/PrezzoServizio/PrezzoServizioDto.cs
+++ src/RegistroServizi.Application/DTOs/PrezzoServizio/PrezzoServizioDto.cs
@@ -1,27 +1,9 @@
 ﻿namespace RegistroServizi.Application.DTOs.PrezzoServizio;
 
 /// <summary>
-/// Data Transfer Object (DTO) representing a PrezzoServizio.
+/// DTO per la rappresentazione dei prezzi dei servizi.
 /// </summary>
-/// <param name="Id"></param>
-/// <param name="TipologiaServizio"></param>
-/// <param name="CostoFisso"></param>
-/// <param name="CostoKm"></param>
-/// <param name="SecondoTrasportato"></param>
-/// <param name="FermoMacchina"></param>
-/// <param name="Accompagnatore"></param>
-/// <param name="ScontoSocio"></param>
-//public record class PrezzoServizioDto(
-//    Guid Id,
-//    TipologiaServizio TipologiaServizio,
-//    decimal CostoFisso,
-//    decimal CostoKm,
-//    decimal SecondoTrasportato,
-//    decimal FermoMacchina,
-//    decimal? Accompagnatore,
-//    int? ScontoSocio);
-
-public record class PrezzoServizioDto
+public class PrezzoServizioDto
 {
     public Guid Id { get; set; }
     public TipologiaServizio TipologiaServizio { get; set; } = default!;

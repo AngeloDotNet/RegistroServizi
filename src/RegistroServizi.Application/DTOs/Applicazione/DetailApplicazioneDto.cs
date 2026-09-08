143ffd2528a51d9157d24397974ee45fc567f828
Angelo Pirola <angelo9871@gmail.com>
2026-09-08 09:48:29 +0200

    Remove DetailApplicazioneDto.cs file content
    Deleted all code, comments, and namespace from DetailApplicazioneDto.cs, including the commented-out record class and its XML documentation.

--- src/RegistroServizi.Application/DTOs/Applicazione/DetailApplicazioneDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RegistroServizi.Application.DTOs.Applicazione;
-
-/// <summary>
-/// Data Transfer Object (DTO) for detailed information about an application.
-/// </summary>
-/// <param name="Id"></param>
-/// <param name="NomeApplicazione"></param>
-/// <param name="Versione"></param>
-//public record class DetailApplicazioneDto(Guid Id, string NomeApplicazione, string Versione);

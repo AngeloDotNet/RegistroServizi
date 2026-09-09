e8c4a4390ed466e8851449854b70606634862210
Angelo Pirola <angelo9871@gmail.com>
2026-09-09 09:19:02 +0200

    Refactor MapPrezzoServizioToDto for new property mapping
    Replaced the original method with a new implementation using object initializer syntax. Updated property mapping: replaced TipologiaServizio with TipologiaServizioId and TipoServizio, mapping TipoServizio from prezzoServizio.TipologiaServizio?.TipoServizio or empty string if null. Other property mappings remain unchanged.

--- src/RegistroServizi.Application/Helpers/PrezzoServizioHelper.cs
+++ src/RegistroServizi.Application/Helpers/PrezzoServizioHelper.cs
@@ -10,10 +10,23 @@ public static class PrezzoServizioHelper
     /// </summary>
     /// <param name="prezzoServizio"></param>
     /// <returns></returns>
-    public static PrezzoServizioDto MapPrezzoServizioToDto(PrezzoServizio prezzoServizio) => new PrezzoServizioDto
+    //public static PrezzoServizioDto MapPrezzoServizioToDto(PrezzoServizio prezzoServizio) => new PrezzoServizioDto
+    //{
+    //    Id = prezzoServizio.Id,
+    //    TipologiaServizioId = prezzoServizio.TipologiaServizioId,
+    //    TipologiaServizio = prezzoServizio.TipologiaServizio,
+    //    CostoFisso = prezzoServizio.CostoFisso,
+    //    CostoKm = prezzoServizio.CostoKm,
+    //    SecondoTrasportato = prezzoServizio.SecondoTrasportato,
+    //    FermoMacchina = prezzoServizio.FermoMacchina,
+    //    Accompagnatore = prezzoServizio.Accompagnatore,
+    //    ScontoSocio = prezzoServizio.ScontoSocio
+    //};
+    public static PrezzoServizioDto MapPrezzoServizioToDto(PrezzoServizio prezzoServizio) => new()
     {
         Id = prezzoServizio.Id,
-        TipologiaServizio = prezzoServizio.TipologiaServizio,
+        TipologiaServizioId = prezzoServizio.TipologiaServizioId,
+        TipoServizio = prezzoServizio.TipologiaServizio?.TipoServizio ?? string.Empty,
         CostoFisso = prezzoServizio.CostoFisso,
         CostoKm = prezzoServizio.CostoKm,
         SecondoTrasportato = prezzoServizio.SecondoTrasportato,

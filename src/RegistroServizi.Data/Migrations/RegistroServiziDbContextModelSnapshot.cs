de3d4c462ba54d19f4aafb5b2e0d3e75cafbf912
Angelo Pirola <angelo9871@gmail.com>
2026-09-10 10:00:23 +0200

    Explicitly set schema to null for AspNetUserPasskeys
    Changed ToTable mapping for "AspNetUserPasskeys" to explicitly set the schema to null, making the schema assignment explicit rather than relying on the default behavior.

--- src/RegistroServizi.Data/Migrations/RegistroServiziDbContextModelSnapshot.cs
+++ src/RegistroServizi.Data/Migrations/RegistroServiziDbContextModelSnapshot.cs
@@ -417,7 +417,7 @@ namespace RegistroServizi.Data.Migrations
 
                             b1.HasKey("IdentityUserPasskeyCredentialId");
 
-                            b1.ToTable("AspNetUserPasskeys");
+                            b1.ToTable("AspNetUserPasskeys", (string)null);
 
                             b1
                                 .ToJson("Data")

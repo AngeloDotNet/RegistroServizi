0e9b67a2d6c3c09fabede2107a5057fff875ddfa
Angelo Pirola <angelo9871@gmail.com>
2026-09-11 00:43:03 +0200

    Add global usings for common namespaces project-wide
    Added global using directives to GlobalUsings.cs for ASP.NET Core Identity, EF Core, Microsoft.Extensions, and app/domain namespaces to reduce repetitive using statements across files.

---
+++ src/RegistroServizi.Data/GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.Migrations;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using RegistroServizi.Application.Interfaces.Persistence;
+global using RegistroServizi.Data.Identity;
+global using RegistroServizi.Domain.Entities;
+global using RegistroServizi.Domain.Enums;

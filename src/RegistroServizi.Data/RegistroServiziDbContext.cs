edb0d27f23b16c7f41a28b795c9fb74454d5cb91
Angelo Pirola <angelo9871@gmail.com>
2026-09-11 00:43:31 +0200

    Refactor RegistroServiziDbContext for clarity and docs
    Added XML docs, implemented IRegistroServiziDbContext, and explicitly exposed DbSet properties for main entities. Enhanced OnModelCreating documentation and applied entity configurations from the current assembly to improve maintainability and usability.

--- src/RegistroServizi.Data/RegistroServiziDbContext.cs
+++ src/RegistroServizi.Data/RegistroServiziDbContext.cs
@@ -1,12 +1,20 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using RegistroServizi.Data.Identity;
-
 namespace RegistroServizi.Data;
 
-public class RegistroServiziDbContext(DbContextOptions<RegistroServiziDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
+/// <summary>
+/// Represents the database context for the RegistroServizi application, inheriting from IdentityDbContext to include identity management features.
+/// </summary>
+/// <param name="options"></param>
+public class RegistroServiziDbContext(DbContextOptions<RegistroServiziDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options), IRegistroServiziDbContext
 {
+    public virtual DbSet<TipologiaServizio> TipologieServizio => Set<TipologiaServizio>();
+    public virtual DbSet<PrezzoServizio> PrezziServizi => Set<PrezzoServizio>();
+    public virtual DbSet<Applicazione> Applicazioni => Set<Applicazione>();
+    public virtual DbSet<Personalizzazione> Personalizzazioni => Set<Personalizzazione>();
+
+    /// <summary>
+    /// Override the OnModelCreating method to apply entity configurations from the assembly containing the RegistroServiziDbContext class.
+    /// </summary>
+    /// <param name="modelBuilder"></param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);

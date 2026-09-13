3f47a002b46103652105ac98ed49ac90e4b35be7
Angelo Pirola <angelo9871@gmail.com>
2026-09-13 16:48:01 +0200

    Update global usings and adjust using order
    Added global usings for System.Collections.Concurrent, Microsoft.EntityFrameworkCore, and Microsoft.Extensions.Caching.Memory. Removed the global using for RegistroServizi.Domain.Enums and reordered some using statements for consistency.

--- src/RegistroServizi.Application/GlobalUsings.cs
+++ src/RegistroServizi.Application/GlobalUsings.cs
@@ -1,11 +1,13 @@
-﻿global using Microsoft.AspNetCore.Http;
+﻿global using System.Collections.Concurrent;
+global using Microsoft.AspNetCore.Http;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Caching.Memory;
 global using Microsoft.Extensions.DependencyInjection;
 global using RegistroServizi.Application.DTOs.Applicazione;
 global using RegistroServizi.Application.DTOs.PrezzoServizio;
 global using RegistroServizi.Application.Helpers;
+global using RegistroServizi.Application.Interfaces.Common;
 global using RegistroServizi.Application.Interfaces.Persistence;
 global using RegistroServizi.Application.Interfaces.Services;
 global using RegistroServizi.Application.Services;
 global using RegistroServizi.Domain.Entities;
-global using RegistroServizi.Domain.Enums;

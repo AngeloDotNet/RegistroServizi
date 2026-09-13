43f57d3538a7e492b25924e6ed443f8e69c25aa6
Angelo Pirola <angelo9871@gmail.com>
2026-09-13 16:48:49 +0200

    Add StatiBolla entity configuration class
    Created StatiBollaConfiguration to map StatoBolla entity to "StatiBolla" table, set Id as primary key, enforce unique Descrizione, and configure Descrizione as required with max length 200.

---
+++ src/RegistroServizi.Data/Configurations/StatiBollaConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace RegistroServizi.Data.Configurations;
+
+public class StatiBollaConfiguration : IEntityTypeConfiguration<StatoBolla>
+{
+    public void Configure(EntityTypeBuilder<StatoBolla> builder)
+    {
+        builder.ToTable("StatiBolla");
+
+        builder.HasKey(a => a.Id);
+
+        builder.HasIndex(a => a.Descrizione)
+            .IsUnique();
+
+        builder.Property(a => a.Descrizione)
+            .IsRequired()
+            .HasMaxLength(200);
+    }
+}

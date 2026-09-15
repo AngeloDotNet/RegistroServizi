7bd7d94a29be815bfa5c76736ca17d6f2044eff3
Angelo Pirola <angelo9871@gmail.com>
2026-09-15 15:50:11 +0200

    Add EF Core configs for StatoBolla and TitoloStudio entities
    Added StatoBollaConfiguration and TitoloStudioConfiguration classes. Both map entities to specific tables, set Id as primary key, enforce unique index on Descrizione, and require Descrizione to be non-null with max length 200.

---
+++ src/RegistroServizi.Data/Configurations/TitoloStudioConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace RegistroServizi.Data.Configurations;
+
+internal class TitoloStudioConfiguration : IEntityTypeConfiguration<TitoloStudio>
+{
+    public void Configure(EntityTypeBuilder<TitoloStudio> builder)
+    {
+        builder.ToTable("TitoliStudio");
+
+        builder.HasKey(a => a.Id);
+
+        builder.HasIndex(a => a.Descrizione)
+            .IsUnique();
+
+        builder.Property(a => a.Descrizione)
+            .IsRequired()
+            .HasMaxLength(200);
+    }
+}

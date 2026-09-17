cbdfcec54c33cc47f8dcfc297c82dca0d5e6c405
Angelo Pirola <angelo9871@gmail.com>
2026-09-17 10:15:39 +0200

    Add DependencyInjection class for options validation
    Introduced a static DependencyInjection class in RegistroServizi.Web with a generic ConfigureAndValidate<TOptions> method. This method configures, binds, and validates options using dependency injection, ensuring validation at application startup.

---
+++ src/RegistroServizi.Web/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistroServizi.Web;
+
+public static class DependencyInjection
+{
+    public static void ConfigureAndValidate<TOptions>(string section)
+        => builder.Services.AddOptions<TOptions>()
+            .Bind(builder.Configuration.GetSection(section))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+}

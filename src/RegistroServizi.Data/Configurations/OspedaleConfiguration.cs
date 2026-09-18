e6771bb5d05f4e4c9e32c34ef662b0b73119d63c
Angelo Pirola <angelo9871@gmail.com>
2026-09-18 09:58:55 +0200

    Reformat config code for improved readability
    Reformatted the configuration code by splitting chained method calls onto separate lines for better readability. No functional changes were made; only code style and formatting were improved for clarity.

--- src/RegistroServizi.Data/Configurations/OspedaleConfiguration.cs
+++ src/RegistroServizi.Data/Configurations/OspedaleConfiguration.cs
@@ -8,28 +8,38 @@ public class OspedaleConfiguration : IEntityTypeConfiguration<Ospedale>
 
         builder.HasKey(o => o.Id);
 
-        builder.HasIndex(o => o.NomeOspedale).IsUnique();
+        builder.HasIndex(o => o.NomeOspedale)
+            .IsUnique();
 
-        builder.Property(o => o.NomeOspedale).IsRequired().HasMaxLength(200);
+        builder.Property(o => o.NomeOspedale)
+            .IsRequired()
+            .HasMaxLength(200);
 
         builder.ComplexProperty(o => o.Indirizzo, indirizzo =>
         {
-            indirizzo.Property(i => i.Strada).IsRequired().HasMaxLength(200);
-            indirizzo.Property(i => i.Citta).IsRequired().HasMaxLength(100);
-            indirizzo.Property(i => i.Provincia).IsRequired().HasMaxLength(100);
-            //indirizzo.Property(i => i.Cap).IsRequired();
-            indirizzo.Property(i => i.Cap).HasDefaultValue(0);
+            indirizzo.Property(i => i.Strada)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            indirizzo.Property(i => i.Citta)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            indirizzo.Property(i => i.Provincia)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            indirizzo.Property(i => i.Cap)
+                .HasDefaultValue(0);
         });
 
         builder.ComplexProperty(o => o.Coordinate, coordinate =>
         {
-            //coordinate.Property(c => c.Latitudine).IsRequired();
-            //coordinate.Property(c => c.Longitudine).IsRequired();
-            coordinate.Property(c => c.Latitudine).HasDefaultValue(0.0);
-            coordinate.Property(c => c.Longitudine).HasDefaultValue(0.0);
-        });
+            coordinate.Property(c => c.Latitudine)
+                .HasDefaultValue(0.0);
 
-        //builder.OwnsOne(o => o.Indirizzo);
-        //builder.OwnsOne(o => o.Coordinate);
+            coordinate.Property(c => c.Longitudine)
+                .HasDefaultValue(0.0);
+        });
     }
 }

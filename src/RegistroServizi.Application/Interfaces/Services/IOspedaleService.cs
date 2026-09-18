b72c0e1aaac65bf2867d8fcc771435a8f9071aa2
Angelo Pirola <angelo9871@gmail.com>
2026-09-18 09:50:42 +0200

    Add IOspedaleService interface for async CRUD operations
    Introduced IOspedaleService in RegistroServizi.Application.Interfaces.Services to define async CRUD methods for Ospedale entities, supporting cancellation tokens.

---
+++ src/RegistroServizi.Application/Interfaces/Services/IOspedaleService.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistroServizi.Application.Interfaces.Services;
+
+public interface IOspedaleService
+{
+    Task<IReadOnlyList<OspedaleDto>> GetAllOspedaliAsync(CancellationToken cancellationToken = default);
+    Task<OspedaleDto> GetByIdOspedaleAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<OspedaleDto> CreateOspedaleAsync(CreateOspedaleDto createDto, CancellationToken cancellationToken = default);
+    Task<OspedaleDto> UpdateOspedaleAsync(UpdateOspedaleDto updateDto, CancellationToken cancellationToken = default);
+    Task DeleteOspedaleAsync(Guid id, CancellationToken cancellationToken = default);
+}

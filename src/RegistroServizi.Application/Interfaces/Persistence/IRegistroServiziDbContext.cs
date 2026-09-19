fd30de6046140b64d97dc67a58cee3a248b99bb8
Angelo Pirola <angelo9871@gmail.com>
2026-09-19 10:18:31 +0200

    Rewrite and clarify IRegistroServiziDbContext docs
    Improved XML documentation for IRegistroServiziDbContext and its members. Enhanced clarity on interface purpose, described DbSet properties, and detailed SaveChangesAsync and Entry methods. Added remarks on dependency injection, unit testing, and transactions. Condensed redundant comments and updated parameter/return descriptions for accuracy.

--- src/RegistroServizi.Application/Interfaces/Persistence/IRegistroServiziDbContext.cs
+++ src/RegistroServizi.Application/Interfaces/Persistence/IRegistroServiziDbContext.cs
@@ -3,9 +3,12 @@
 namespace RegistroServizi.Application.Interfaces.Persistence;
 
 /// <summary>
-/// Represents the database context for the RegistroServizi application.
-/// This interface defines the contract for interacting with the underlying database, including access to the various DbSet properties representing the entities in the application.
+/// Abstraction of an Entity Framework Core DbContext for the Registro servizi domain, exposing DbSet properties for
+/// domain entities and EF Core operations for change tracking and persistence.
 /// </summary>
+/// <remarks>Implementations should provide DbSet<T> properties for TipologiaServizio, PrezzoServizio,
+/// Applicazione, StatoBolla, TitoloStudio and Ospedale, and implement SaveChangesAsync and Entry to support dependency
+/// injection, unit testing, and mocking of data access.</remarks>
 public interface IRegistroServiziDbContext
 {
     DbSet<TipologiaServizio> TipologieServizio { get; }
@@ -16,18 +19,21 @@ public interface IRegistroServiziDbContext
     DbSet<Ospedale> Ospedali { get; }
 
     /// <summary>
-    /// Saves all changes made in this context to the database asynchronously.
-    /// This method will automatically detect changes made to the tracked entities and generate the appropriate SQL commands to persist those changes to the database.
+    /// Saves all changes made in the context to the underlying data store asynchronously.
     /// </summary>
-    /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-    /// <returns>A task that represents the asynchronous save operation. The task result contains the number of state entries written to the database.</returns>
+    /// <remarks>Operations are executed in a single transaction when supported by the provider.</remarks>
+    /// <param name="cancellationToken">Token to cancel the asynchronous save operation.</param>
+    /// <returns>A task that represents the asynchronous save operation. The task result contains the number of state entries
+    /// written to the underlying store.</returns>
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Gets an EntityEntry for the given entity. This method provides access to change tracking information and operations for the specified entity.
+    /// Gets an EntityEntry for the specified entity that provides access to change-tracking information and operations.
     /// </summary>
-    /// <typeparam name="TEntity">The type of the entity.</typeparam>
-    /// <param name="entity">The entity for which to get the EntityEntry.</param>
-    /// <returns>An EntityEntry for the specified entity.</returns>
+    /// <remarks>Throws ArgumentNullException if entity is null. If the entity is not tracked, the returned
+    /// entry can be used to begin tracking and to set the entity state.</remarks>
+    /// <typeparam name="TEntity">The entity type.</typeparam>
+    /// <param name="entity">The entity for which to obtain the EntityEntry.</param>
+    /// <returns>An EntityEntry<TEntity> that provides access to change-tracking information and operations for the entity.</returns>
     EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
 }

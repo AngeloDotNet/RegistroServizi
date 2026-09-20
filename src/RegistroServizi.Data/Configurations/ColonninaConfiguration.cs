3b2752fae3a7904002f65be9a204ca5fe7ad870c
Angelo Pirola <angelo9871@gmail.com>
2026-09-20 09:51:03 +0200

    Add ColonninaConfiguration for EF Core entity setup
    Created ColonninaConfiguration to configure the Colonnina entity:
    - Set table name to "Colonnine"
    - Defined primary key and index on NomeColonnina
    - Configured required properties with max lengths
    - Set up complex property Coordinate with default values

---
+++ src/RegistroServizi.Data/Configurations/ColonninaConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace RegistroServizi.Data.Configurations;
+
+public class ColonninaConfiguration : IEntityTypeConfiguration<Colonnina>
+{
+    public void Configure(EntityTypeBuilder<Colonnina> builder)
+    {
+        builder.ToTable("Colonnine");
+
+        builder.HasKey(c => c.Id);
+
+        builder.HasIndex(a => a.NomeColonnina);
+
+        builder.Property(c => c.NomeColonnina)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(c => c.Comune)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(c => c.Provincia)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.ComplexProperty(o => o.Coordinate, coordinate =>
+        {
+            coordinate.Property(c => c.Latitudine)
+                .HasDefaultValue(0.0);
+
+            coordinate.Property(c => c.Longitudine)
+                .HasDefaultValue(0.0);
+        });
+    }
+}

600461896f04cfee64402fd3e22832423a9b5845
Angelo Pirola <angelo9871@gmail.com>
2026-09-22 09:29:21 +0200

    Refactor NavigationMenu to use IOptionsMonitor for config
    Replaces database queries for app name/version in NavigationMenu with values from configuration via IOptionsMonitor<ApplicazioneOptions>. Moves related logic to NavigationMenu.razor.cs partial class, implements IDisposable for options subscription cleanup, and updates markup to use new fields. Comments out old database access code.

---
+++ src/RegistroServizi.Web/Components/Layout/NavigationMenu.razor.cs
@@ -0,0 +1,71 @@
+﻿namespace RegistroServizi.Web.Components.Layout;
+
+public partial class NavigationMenu
+{
+    // private string appName = string.Empty;
+    // private string version = string.Empty;
+
+    // private bool _loaded;
+
+    // protected override async Task OnInitializedAsync()
+    // {
+    //     if (_loaded)
+    //     {
+    //         return;
+    //     }
+
+    //     _loaded = true;
+
+    //     await using var dbContext = await DbContextFactory.CreateDbContextAsync();
+
+    //     var applicazione = await dbContext.Applicazioni
+    //         .OrderBy(x => x.Id)
+    //         .Select(x => new { x.NomeApplicazione, x.Versione })
+    //         .FirstOrDefaultAsync();
+
+    //     if (applicazione is not null)
+    //     {
+    //         appName = applicazione.NomeApplicazione;
+    //         version = applicazione.Versione;
+    //     }
+    // }
+
+    private string? _nomeApplicazione;
+    private string? _versioneApplicazione;
+
+    private IDisposable? _optionsChangeSubscription;
+
+    protected override void OnInitialized()
+    {
+        // _nomeAssociazione = AssociazioneOptions.CurrentValue.NomeAssociazione;
+        _nomeApplicazione = ApplicazioneOptions.CurrentValue.NomeApplicazione;
+        _versioneApplicazione = ApplicazioneOptions.CurrentValue.VersioneApplicazione;
+
+        // _optionsChangeSubscription = AssociazioneOptions.OnChange(options =>
+        // {
+        //     _nomeAssociazione = options.NomeAssociazione;
+        //     _ = InvokeAsync(StateHasChanged);
+        // });
+
+        _optionsChangeSubscription = ApplicazioneOptions.OnChange(options =>
+        {
+            _nomeApplicazione = options.NomeApplicazione;
+            _versioneApplicazione = options.VersioneApplicazione;
+            _ = InvokeAsync(StateHasChanged);
+        });
+
+        // NavigationManager.LocationChanged += OnLocationChanged;
+    }
+
+    public void Dispose()
+    {
+        // NavigationManager.LocationChanged -= OnLocationChanged;
+        _optionsChangeSubscription?.Dispose();
+    }
+
+    //private void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+    //{
+    //    currentUrl = NavigationManager.ToBaseRelativePath(e.Location);
+    //    StateHasChanged();
+    //}
+}

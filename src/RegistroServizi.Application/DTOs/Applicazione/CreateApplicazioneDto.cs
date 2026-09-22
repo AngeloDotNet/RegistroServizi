75d2b1fdba6306fc4119abd19f088ff14b390b58
Angelo Pirola <angelo9871@gmail.com>
2026-09-22 09:23:59 +0200

    Remove Applicazione DTO classes and definitions
    Deleted ApplicazioneDto.cs, CreateApplicazioneDto.cs, and UpdateApplicazioneDto.cs, removing all related DTO classes, properties, constructors, and comments. These DTOs are no longer available for data transfer operations.

--- src/RegistroServizi.Application/DTOs/Applicazione/CreateApplicazioneDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RegistroServizi.Application.DTOs.Applicazione;
-
-/// <summary>
-/// Data Transfer Object (DTO) for creating a new application.
-/// </summary>
-/// <param name="NomeApplicazione"></param>
-/// <param name="Versione"></param>
-/// <param name="TimeZone"></param>
-public record class CreateApplicazioneDto(string NomeApplicazione, string Versione, string TimeZone);

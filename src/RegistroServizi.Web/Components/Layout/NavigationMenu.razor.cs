2bf1225e0b1294d37c914fe2dca6a06bb91004e0
Angelo Pirola <angelo9871@gmail.com>
2026-09-23 09:14:13 +0200

    Remove old commented code from NavigationMenu components
    Cleaned up NavigationMenu.razor and NavigationMenu.razor.cs by removing outdated commented-out logic for loading application name/version and handling options changes. Retained only the necessary logic for subscribing to ApplicazioneOptions changes and updating component state.

--- src/RegistroServizi.Web/Components/Layout/NavigationMenu.razor.cs
+++ src/RegistroServizi.Web/Components/Layout/NavigationMenu.razor.cs
@@ -2,34 +2,6 @@
 
 public partial class NavigationMenu
 {
-    // private string appName = string.Empty;
-    // private string version = string.Empty;
-
-    // private bool _loaded;
-
-    // protected override async Task OnInitializedAsync()
-    // {
-    //     if (_loaded)
-    //     {
-    //         return;
-    //     }
-
-    //     _loaded = true;
-
-    //     await using var dbContext = await DbContextFactory.CreateDbContextAsync();
-
-    //     var applicazione = await dbContext.Applicazioni
-    //         .OrderBy(x => x.Id)
-    //         .Select(x => new { x.NomeApplicazione, x.Versione })
-    //         .FirstOrDefaultAsync();
-
-    //     if (applicazione is not null)
-    //     {
-    //         appName = applicazione.NomeApplicazione;
-    //         version = applicazione.Versione;
-    //     }
-    // }
-
     private string? _nomeApplicazione;
     private string? _versioneApplicazione;
 
@@ -37,15 +9,8 @@ public partial class NavigationMenu
 
     protected override void OnInitialized()
     {
-        // _nomeAssociazione = AssociazioneOptions.CurrentValue.NomeAssociazione;
         _nomeApplicazione = ApplicazioneOptions.CurrentValue.NomeApplicazione;
         _versioneApplicazione = ApplicazioneOptions.CurrentValue.VersioneApplicazione;
-
-        // _optionsChangeSubscription = AssociazioneOptions.OnChange(options =>
-        // {
-        //     _nomeAssociazione = options.NomeAssociazione;
-        //     _ = InvokeAsync(StateHasChanged);
-        // });
 
         _optionsChangeSubscription = ApplicazioneOptions.OnChange(options =>
         {
@@ -53,19 +18,10 @@ public partial class NavigationMenu
             _versioneApplicazione = options.VersioneApplicazione;
             _ = InvokeAsync(StateHasChanged);
         });
-
-        // NavigationManager.LocationChanged += OnLocationChanged;
     }
 
     public void Dispose()
     {
-        // NavigationManager.LocationChanged -= OnLocationChanged;
         _optionsChangeSubscription?.Dispose();
     }
-
-    //private void OnLocationChanged(object? sender, LocationChangedEventArgs e)
-    //{
-    //    currentUrl = NavigationManager.ToBaseRelativePath(e.Location);
-    //    StateHasChanged();
-    //}
 }

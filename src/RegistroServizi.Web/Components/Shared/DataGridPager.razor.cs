82d938bbd385c203175c39ab2c8b27f9f7b795db
Angelo Pirola <angelo9871@gmail.com>
2026-09-25 11:26:32 +0200

    Move DataGridPager logic to code-behind
    Refactor `DataGridPager` into a partial class code-behind, moving its parameters and `EffectivePageSizeOptions` calculation from the Razor file while preserving defaults and behavior.

---
+++ src/RegistroServizi.Web/Components/Shared/DataGridPager.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace RegistroServizi.Web.Components.Shared;
+
+public partial class DataGridPager
+{
+    [Parameter, EditorRequired] public MudBlazorDataGridOptions DataGridOptions { get; set; } = default!;
+
+    [Parameter] public string RowsPerPageString { get; set; } = "Righe per pagina:";
+    [Parameter] public string InfoFormat { get; set; } = "Righe {first_item} - {last_item} di {all_items}";
+
+    [Parameter] public int[]? PageSizeOptions { get; set; }
+
+    [Parameter] public bool PageSizeSelector { get; set; } = true;
+    [Parameter] public bool ShowNavigation { get; set; } = true;
+    [Parameter] public bool ShowPageNumber { get; set; } = false;
+
+    private int[] EffectivePageSizeOptions => PageSizeOptions ?? DataGridOptions.PageSize;
+}
